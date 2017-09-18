e237b2b93d9202be0278f2b897f8fce453905d7f
Chris Holdgraf <choldgraf@berkeley.edu>
2017-09-18 11:00:51 -0700

    update jsonpointer

--- setup.py
+++ setup.py
@@ -11,17 +11,17 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='sphinx-jsonschema',
     version='1.3',
-    
+
     description='Sphinx extension to display JSON Schema',
     long_description=long_description,
     url='https://github.com/lnoor/sphinx-jsonschema',
-    
+
     author='Leo Noordergraaf',
     author_email='leo@noordergraaf.net',
-    
+
     license='GPLv3',
     platforms='any',
-    
+
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Plugins',
@@ -34,13 +34,12 @@ setup(
         'Topic :: Software Development :: Documentation',
         'Topic :: Documentation :: Sphinx'
     ],
-    
+
     keywords='sphinx json schema',
     packages=find_packages(),
     package_data={
         '': ['LICENSE']
     },
-    
-    install_requires=['docutils', 'requests', 'json-pointer']
+
+    install_requires=['docutils', 'requests', 'jsonpointer']
 )
-        

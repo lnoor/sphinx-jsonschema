6dcce5304e66010d642e03b47cc65f2b124af49e
Leo Noordergraaf <leo@noordergraaf.net>
2021-06-20 16:32:11 +0200

    releasing 1.16.9

--- sphinx-jsonschema/__init__.py
+++ sphinx-jsonschema/__init__.py
@@ -208,5 +208,5 @@ def setup(app):
     app.add_config_value('jsonschema_options', {}, 'env')
     return {
         'parallel_read_safe': True,
-        'version': '1.16.8'
+        'version': '1.16.9'
     }
